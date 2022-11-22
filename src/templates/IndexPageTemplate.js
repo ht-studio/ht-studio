d35a86ebdbb501692fdfbaebef46f44d2d448368
sailfuture <78326976+sailfuture@users.noreply.github.com>
2022-11-22 12:37:59 -0500

    Update IndexPageTemplate.js

--- src/templates/IndexPageTemplate.js
+++ src/templates/IndexPageTemplate.js
@@ -80,18 +80,4 @@ const IndexPageTemplate = ({
   )
 }
 
-<script>
-  if (window.netlifyIdentity) {
-    window.netlifyIdentity.on("init", user => {
-      if (!user) {
-        window.netlifyIdentity.on("login", () => {
-          document.location.href = "/admin/";
-        });
-      }
-    });
-  }
-</script>
-
-<script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
-
 export default IndexPageTemplate
